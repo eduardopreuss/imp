730694bfb530f35eb337f105fb3851ba8a979900
Renan Kummer <renankj@gmail.com>
2017-10-11 16:13:54 -0300

    Added findBy methods

--- src/main/java/com/imp/repositories/ProgramRepository.java
+++ src/main/java/com/imp/repositories/ProgramRepository.java
@@ -5,6 +5,7 @@
  */
 package com.imp.repositories;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import com.imp.entities.Program;
 
@@ -16,6 +17,35 @@ import com.imp.entities.Program;
  */
 public interface ProgramRepository extends CrudRepository<Program, Integer> {
 
-	// TODO: Define methods.
+	/**
+	 * Finds all programs.
+	 * 
+	 * @return List of all programs.
+	 */
+	public List<Program> findAll();
+
+	/**
+	 * Finds a program by its id.
+	 * 
+	 * @param id ID of program to find.
+	 * @return Program if found, null otherwise.
+	 */
+	public Program findById(int id);
+
+	/**
+	 * Finds a program by its official name.
+	 * 
+	 * @param name Name of program to find.
+	 * @return Program if found, null otherwise.
+	 */
+	public Program findByName(String name);
+
+	/**
+	 * Finds a program by its alias.
+	 * 
+	 * @param alias Alias of program to find.
+	 * @return Program if found, null otherwise.
+	 */
+	public Program findByAlias(String alias);
 
 }
