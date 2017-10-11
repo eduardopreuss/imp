730fee488ddbc50c9cffe3b763f875c34b9ed11e
Renan Kummer <renankj@gmail.com>
2017-10-11 18:31:26 -0300

    Added findBy methods

--- src/main/java/com/imp/services/ProgramService.java
+++ src/main/java/com/imp/services/ProgramService.java
@@ -1,20 +1,90 @@
+/**
+ * Internship Management Program
+ * 
+ * @since Oct 10th, 2017
+ */
 package com.imp.services;
 
 import java.time.LocalDate;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import com.imp.entities.Program;
 import com.imp.exceptions.StartDateAfterEndDateException;
 import com.imp.repositories.ProgramRepository;
 
-public class ProgramService{
-	
-	ProgramRepository programRepository;
-	
+
+/**
+ * Service for Program class.
+ * 
+ * @author IT Academy #3
+ */
+public class ProgramService {
+
+	private ProgramRepository programRepository;
+
+	/**
+	 * Constructs ProgramService using dependency injection of a ProgramRepository.
+	 * 
+	 * @param programRepository Repository of programs.
+	 */
 	@Autowired
-	public void addProgram(String title, String description, String ownerBadge, LocalDate startDate, LocalDate endDate) throws StartDateAfterEndDateException{
-		Program program = new Program(title, description, ownerBadge, startDate, endDate); 
+	public ProgramService(ProgramRepository programRepository) {
+		this.programRepository = programRepository;
+	}
+
+	/**
+	 * Creates a program and save it in the repository.
+	 * 
+	 * @param title Program's title.
+	 * @param description Program's description.
+	 * @param ownerBadge Program owner's badge.
+	 * @param startDate Program's start date.
+	 * @param endDate Program's end date.
+	 * @throws StartDateAfterEndDateException
+	 */
+	public void addProgram(	String title, String description, String ownerBadge, LocalDate startDate,
+							LocalDate endDate) throws StartDateAfterEndDateException {
+		Program program = new Program(title, description, ownerBadge, startDate, endDate);
 		programRepository.save(program);
-	}		
+	}
+
+	/**
+	 * Finds all programs.
+	 * 
+	 * @return List of all programs.
+	 */
+	public List<Program> findAll() {
+		return this.programRepository.findAll();
+	}
+
+	/**
+	 * Finds a program by its ID.
+	 * 
+	 * @param id ID of program to find.
+	 * @return Program if found, null otherwise.
+	 */
+	public Program findById(int id) {
+		return this.programRepository.findById(id);
+	}
+
+	/**
+	 * Finds a program by its official title.
+	 * 
+	 * @param title Title of program to find.
+	 * @return Program if found, null otherwise.
+	 */
+	public Program findByTitle(String title) {
+		return this.programRepository.findByTitle(title);
+	}
+
+	/**
+	 * Finds a program by its owner's badge.
+	 * 
+	 * @param ownerBadge Owner's badge of program to find.
+	 * @return Program if found, null otherwise.
+	 */
+	public Program findByOwnerBadge(String ownerBadge) {
+		return this.programRepository.findByOwnerBadge(ownerBadge);
+	}
+
 }
