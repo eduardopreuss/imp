33290f8766af3399a696712ae5acbb11023b75d9
Anderson_Muniz <Anderson_Muniz@FLEXW10VMAM-442.vcp.amer.dell.com>
2017-10-13 16:01:01 -0300

    Up

--- src/main/java/com/imp/controllers/UserController.java
+++ src/main/java/com/imp/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.imp.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.imp.entities.User;
-import com.imp.repositories.UserRepository;
 import com.imp.services.UserService;
 
 @RestController
