721e251d8d30c16c35c35b0f78059003b19d2394
Moura <Erick_Moura@DellTeam.com>
2017-10-16 11:35:10 -0200

    Cleaning code, hardcoding data example of program List

--- src/app/layout/tables/tables.component.ts
+++ src/app/layout/tables/tables.component.ts
@@ -11,10 +11,40 @@ export class TablesComponent implements OnInit {
 
     tableHeader: string[];
     thFontWeight: string;
+    program: object[];
 
     constructor() { 
         this.tableHeader = ['Title', 'Description', 'Owner', 'Start Date', 'End Date'];
-        
+
+        this.program = [{
+            title: 'Dell IT Academy',
+            description: 'Program developed for the purpose of hiring new interns',
+            owner: 'Muller, Nelson',
+            startDate: '08/01/2017',
+            endDate: '01/31/2018'
+        },
+        {
+            title: 'U@Dell',
+            description: 'Talent Aquisition Program',
+            owner: 'Fernandes, Gabriela',
+            startDate: '01/01/2017',
+            endDate: '12/31/2017'
+        },
+        {
+            title: 'Tests Program',
+            description: 'Program for testing software',
+            owner: 'Callegari, Daniel',
+            startDate: '01/01/2017',
+            endDate: '12/31/2017'
+        },
+        {
+            title: 'DevTeamCoop',
+            description: 'Software development program created for undergraduate students!!!',
+            owner: 'Dell, Michael',
+            startDate: '01/01/2017',
+            endDate: '12/31/2017'
+        }
+    ];
     }
     ngOnInit() { }
 }
