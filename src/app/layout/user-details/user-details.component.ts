60439db01449beb8e3a6ed3651d67ebdfdd8b7ea
Moura <Erick_Moura@DellTeam.com>
2017-10-26 17:34:40 -0200

    implementing tabs

--- src/app/layout/user-details/user-details.component.ts
+++ src/app/layout/user-details/user-details.component.ts
@@ -6,7 +6,41 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
+
+    tableHeader: string[];
+    program: object[];
+
     constructor() {
+        this.tableHeader = ['Title', 'Start Date', 'End Date', 'Role', 'Status'];
+
+        this.program = [{
+            title: 'Dell IT Academy',
+            startDate: '08/01/2017',
+            endDate: '01/31/2018',
+            role: 'Coordinator',
+            status: 'Active'
+        },
+        {
+            title: 'U@Dell',
+            startDate: '01/01/2017',
+            endDate: '12/31/2017',
+            role: 'Coordinator',
+            status: 'Active'
+        },
+        {
+            title: 'Tests Program',
+            startDate: '01/01/2017',
+            endDate: '12/31/2017',
+            role: 'Coordinator',
+            status: 'Active'
+        },
+        {
+            title: 'DevTeamCoop',
+            startDate: '01/01/2017',
+            endDate: '12/31/2017',
+            role: 'Coordinator',
+            status: 'Active'
+        }]
     }
 
     ngOnInit() {
--- src/app/layout/user-details/user-details.module.ts
+++ src/app/layout/user-details/user-details.module.ts
@@ -1,13 +1,16 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UserDetailsRoutingModule } from './user-details-routing.module';
 import { UserDetailsComponent } from './user-details.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    UserDetailsRoutingModule
+    UserDetailsRoutingModule,
+    NgbModule
   ],
   declarations: [UserDetailsComponent]
 })
